be3e41ef04347c70f095a561bbcda02a7d4855da
sharon287 <sharonabraham2029@cs.sjcetpalai.ac.in>
2026-08-02 20:48:34 +0530

    Create OddNumbers.java

---
+++ OddNumbers.java
@@ -0,0 +1,24 @@
+package mod1;
+import java.util.Scanner;
+
+public class OddNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 2 == 0) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+
+        sc.close();
+    }
+}
